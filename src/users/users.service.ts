d2be78424141bff60df6321a33979c68bb55ec8d
Nathan Schrader <natecschrader@gmail.com>
2024-08-09 20:10:20 -0700

    add constr fn, InjectRepository decorater, create and save user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+
+  create(email: string, password: string) {
+    const user = this.repo.create({ email, password });
+
+    return this.repo.save(user);
+  }
+}
+
+// 1. argument name is repo
+// 2. private added to abbreviate prop definition and assignment
+// 3. type annotation is Repository, applied generic type of type User
+// 4. InjectRepository tells dependency inj system that we need User Repository
