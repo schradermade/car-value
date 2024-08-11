b931bd83c32ee140d0e55b5210ebd31fc6e24558
Nathan Schrader <natecschrader@gmail.com>
2024-08-10 20:53:20 -0700

    add AfterInsert/Update/Remove decorators to user entity

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,4 +1,11 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  AfterInsert,
+  AfterRemove,
+  AfterUpdate,
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 // Entity decorator tells typeorm it needs to create a table to model this class of users
 @Entity()
@@ -11,4 +18,20 @@ export class User {
 
   @Column()
   password: string;
+
+  // method that runs some logic after a new user into db
+  @AfterInsert()
+  logInsert() {
+    console.log('inserted with user id', this.id);
+  }
+
+  @AfterUpdate()
+  logUpdate() {
+    console.log('UPdated user with id', this.id);
+  }
+
+  @AfterRemove()
+  logRemove() {
+    console.log('removed user with id', this.id);
+  }
 }
